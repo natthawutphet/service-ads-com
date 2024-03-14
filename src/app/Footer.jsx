d6cef49c7fa396c26539429ef2bb3838e67a137b
xxx <xnatthawut1990@gmail.com>
2024-03-14 20:32:34 +0700

    first commit

--- src/app/Footer.jsx
+++ src/app/Footer.jsx
@@ -1,72 +1,63 @@
 import React from 'react'
 import Link from 'next/link';
+import Image from 'next/image';
 
 function Footer() {
   return (
     <>
-       <footer className="page-footer bg-image">
+       <footer className="page-footer">
     <div className="container">
       <div className="row mb-5">
         <div className="col-lg-3 py-3">
           <h3>รายละเอียดบริการ</h3>
-          <p>ค่าบริการ เหมาจ่ารายเดือนเพียงเดือนละ 9,900 บาท ไม่มีค่าใช้ค่าใดใดเพิ่มเติมทั้งสิน
-          </p>
+          <p>บริการรับจ้างทำการตลาดออนไลน์ ที่น่าเชื่อถือที่สุด </p>
+          
+            <Link href="https://lin.ee/JwrDPgA">
 
-          <div className="social-media-button">
-            <Link href="https://lin.ee/JwrDPgA">Yortube</Link>
-          </div>
+              <Image src="/img/yt.jpg" alt="youtube" width={150} height={80} />
+              
+              </Link>
+          
         </div>
         <div className="col-lg-3 py-3">
           <h5>Company</h5>
           <ul className="footer-menu">
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="Google">Google</Link></li>
-            <li><Link href="Facebook">Facebook</Link></li>
-            <li><Link href="course">course</Link></li>
-            <li><Link href="contact">contact</Link></li>
+            <li><Link href="https://www.servicesadss.com/">Home</Link></li>
+            <li><Link href="https://www.servicesadss.com/Google">Google</Link></li>
+            <li><Link href="https://www.servicesadss.com/Facebook">Facebook</Link></li>
+            <li><Link href="https://www.servicesadss.com/Course-Google">คอร์สโฆษณาGoogle</Link></li>
+            <li><Link href="https://www.servicesadss.com/Course-Facebook">คอร์สโฆษณาFacebook</Link></li>
+            <li><Link href="https://www.servicesadss.com/Interesting">สาระน่ารู้</Link></li>
+            <li><Link href="https://www.servicesadss.com/Reviews">รีวิวจากลูกค้าจริง</Link></li>
+            <li><Link href="https://www.servicesadss.com/DowloadsGoogle">LandingPage Googleฟรี</Link></li>
+            <li><Link href="https://www.servicesadss.com/DowloadsFacebook">LandingPage Facebookฟรี</Link></li>
+            <li><Link href="https://www.servicesadss.com/contact">contact</Link></li>
           </ul>
         </div>
         <div className="col-lg-3 py-3">
           <h5>Contact</h5>
-          <p>Ads Manager 32 Soi 20 Charoenrat Road, Khlong San District, Bangkok 10160
-
-          </p>
-          <Link href="#" className="footer-link">0956422872</Link>
-          <Link href="#" className="footer-link">admin@service-ads.com</Link>
+          <p>Ads Manager 32 Soi 20 Charoenrat Road, Khlong San District, Bangkok 10160</p>
+          <Link href="tel:0956422872" className="footer-link">0956422872</Link>
+          <Link href="https://www.servicesadss.com" className="footer-link">admin@servicesadss.com</Link>
         </div>
         <div className="col-lg-3 py-3">
-          <h5>service-ads.com</h5>
-          <form action="/">
+          <h5>www.servicesadss.com</h5>
+          <form action="https://www.servicesadss.com/">
             <input type="text" className="form-control" placeholder="Enter your email.."/>
-            <button type="submit" className="btn btn-success btn-block mt-2">Subscribe</button>
+            <button type="submit" className="btn btn-success btn-block mt-2">Submit</button>
           </form>
         </div>
       </div>
 
-      <p className="text-center" id="copyright">รับจ้างยิงแอดสายเทา &copy; 2024 รับจ้างยิงแอดสายเทา 2024 <Link href="/" target="_blank">รับจ้างยิงแอดสายเทา</Link></p>
+      <p className="text-center" id="copyright">รับจ้างยิงแอดสายเทา &copy; 2024 รับจ้างยิงแอดสายเทา 2024 <Link href="https://www.servicesadss.com" target="_blank">รับจ้างยิงแอดสายเทา</Link></p>
     </div>
-    <p>
-#รับทำ seo สายเทา, #รับทำ seo สายดำ, #แอดสายเทา, #รับยิงแอดสายเทา, #กูเกิ้ลแอดสายเทา, #รับทํา google ads, #สายเทา seo สายเทา, #รับทำโฆษณาสายเทา, #ทำการตลาดสายเทา, #ads สายเทา, #รับทํา seo สายเทา, #ยิงแอดสายเทา, #โปรโมทสายเทา, #facebook ads, #สายเทา, #การตลาดสายเทา, #ยิงadsสายเทา, #ยิงแอด google สายเทา, #รับทำการตลาดสายเทา,
-#รับจ้างยิงแอดสายเทา, #โฆษณาสายเทา, #ตลาดสายเทา, #ads, #สายเทา, #แอดสายเทา, #ยิงแอดสายเทา, #pantip ยิงแอดสายเทาfacebook, #ทําseoสายเทา, #ยิงadsfacebookสายเทา, #ยิงแอดสายเทาเฮียบอย, #รับทำเว็บสายเทา, #การทำseoสายเทา, #ทำseoสายเทา, #ยิงแอดyoutubeสายเทา,# ยิงแอดสายเทาราคา, #ยิงแอดสายเทา2022, #ยิงแอดสายดำ, #รับจ้างแอดสายเทา, 
-#ยิงแอดสายเทาgoogle, #ตลาดสีเทา, #แอดสีเทา, #adsสีเทา, #กราฟฟิกสายเทา, #ยิงแอดเทา, #รับยิงแอดfacebookสายเทา, #รูปยิงแอดสายเทา, #รับจ้างสายเทา, #รับโฆษณาสายเทา 
-#รับยิงads #แอดโฆษณา #สายเทา
-#รับทำการตลาดออนไลน์
-#ยิงads #facebookสายเทา
-#โฆษณาเฟสบุ๊ค
-#รับยิงแอดสายเทา
-#GoogleAds
-#ยิงadsfacebookสายเทา
-#รับยิงads #facebook #สายเทา
-#โฆษณาเฟสบุ๊ค #สายเทาราคาถูก
-#รับยิงแอด #สายเทา #โฆษณา #เฟสบุ๊ค
-#facebookads #สายเทาทำไงคลิก
-#สายเทา #การยิงแอดไห้อยู่รอด
-#ยิงads #สายเทาต้องไงอย่างไรบ้าง
-#ยิงadsปรึกษาเราฟรี 24 ชม
-#adsโฆษณาสายเทา #เฟสบุ๊ค
-#ลงโฆษณาสายเทาแม่นๆสนใจ #คลิกเลย
-#ยิงads #ทีมงานมืออาชีพ #ราคาถูก </p>
+ <p> #รับทำ seo สายเทา, #รับทำ seo สายดำ, #แอดสายเทา, #รับยิงแอดสายเทา, #กูเกิ้ลแอดสายเทา, #รับทํา google ads, #สายเทา seo สายเทา, #รับทำโฆษณาสายเทา, #ทำการตลาดสายเทา, #ads สายเทา, #รับทํา seo สายเทา, #ยิงแอดสายเทา, #โปรโมทสายเทา, #facebook ads, #สายเทา, #การตลาดสายเทา, #ยิงadsสายเทา, #ยิงแอด google สายเทา, #รับทำการตลาดสายเทา,</p>
+<p> #รับจ้างยิงแอดสายเทา, #โฆษณาสายเทา, #ตลาดสายเทา, #ads, #สายเทา, #แอดสายเทา, #ยิงแอดสายเทา, #pantip ยิงแอดสายเทาfacebook, #ทําseoสายเทา, #ยิงadsfacebookสายเทา, #ยิงแอดสายเทาเฮียบอย, #รับทำเว็บสายเทา, #การทำseoสายเทา, #ทำseoสายเทา, #ยิงแอดyoutubeสายเทา,# ยิงแอดสายเทาราคา, #ยิงแอดสายเทา2022, #ยิงแอดสายดำ, #รับจ้างแอดสายเทา, </p>
+<p> #ยิงแอดสายเทาgoogle, #ตลาดสีเทา, #แอดสีเทา, #adsสีเทา, #กราฟฟิกสายเทา, #ยิงแอดเทา, #รับยิงแอดfacebookสายเทา, #รูปยิงแอดสายเทา, #รับจ้างสายเทา, #รับโฆษณาสายเทา  </p>
+<p> #รับยิงads #แอดโฆษณา #สายเทา #รับทำการตลาดออนไลน์ #ยิงads #facebookสายเทา #โฆษณาเฟสบุ๊ค #รับยิงแอดสายเทา #GoogleAds #ยิงadsfacebookสายเทา#รับยิงads #facebook #สายเทา#โฆษณาเฟสบุ๊ค #สายเทาราคาถูก #รับยิงแอด #สายเทา #โฆษณา #เฟสบุ๊ค #facebookads #สายเทาทำไงคลิก #สายเทา #การยิงแอดไห้อยู่รอด #ยิงads #สายเทาต้องไงอย่างไรบ้าง #ยิงadsปรึกษาเราฟรี 24 ชม #adsโฆษณาสายเทา #เฟสบุ๊ค #ลงโฆษณาสายเทาแม่นๆสนใจ #คลิกเลย #ยิงads #ทีมงานมืออาชีพ #ราคาถูก </p>
   </footer>
+
+
     </>
   )
 }
