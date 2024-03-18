eec5be421ee01820ce81883511eef3d3255ffc18
xxx <xnatthawut1990@gmail.com>
2024-03-18 12:55:22 +0700

    first commit

--- src/app/Footer.jsx
+++ src/app/Footer.jsx
@@ -5,14 +5,14 @@ import Image from 'next/image';
 function Footer() {
   return (
     <>
-       <footer className="page-footer">
+       <footer className="page-footer" id='footer'>
     <div className="container">
       <div className="row mb-5">
         <div className="col-lg-3 py-3">
           <h3>รายละเอียดบริการ</h3>
           <p>บริการรับจ้างทำการตลาดออนไลน์ ที่น่าเชื่อถือที่สุด </p>
           
-            <Link href="https://lin.ee/JwrDPgA">
+            <Link  href="/vdo">
 
               <Image src="/img/yt.jpg" alt="youtube" width={150} height={80} />
               
@@ -22,23 +22,23 @@ function Footer() {
         <div className="col-lg-3 py-3">
           <h5>Company</h5>
           <ul className="footer-menu">
-            <li><Link href="https://www.webapp.myfbad.com/">Home</Link></li>
-            <li><Link href="https://www.webapp.myfbad.com/Google">Google</Link></li>
-            <li><Link href="https://www.webapp.myfbad.com/Facebook">Facebook</Link></li>
-            <li><Link href="https://www.webapp.myfbad.com/Course-Google">คอร์สโฆษณาGoogle</Link></li>
-            <li><Link href="https://www.webapp.myfbad.com/Course-Facebook">คอร์สโฆษณาFacebook</Link></li>
-            <li><Link href="https://www.webapp.myfbad.com/Interesting">สาระน่ารู้</Link></li>
-            <li><Link href="https://www.webapp.myfbad.com/Reviews">รีวิวจากลูกค้าจริง</Link></li>
-            <li><Link href="https://www.webapp.myfbad.com/DowloadsGoogle">LandingPage Googleฟรี</Link></li>
-            <li><Link href="https://www.webapp.myfbad.com/DowloadsFacebook">LandingPage Facebookฟรี</Link></li>
-            <li><Link href="https://www.webapp.myfbad.com/contact">contact</Link></li>
+            <li><Link  className="p" href="https://www.webapp.myfbad.com/">Home</Link></li>
+            <li><Link  className="p" href="https://www.webapp.myfbad.com/Google">Google</Link></li>
+            <li><Link  className="p" href="https://www.webapp.myfbad.com/Facebook">Facebook</Link></li>
+            <li><Link  className="p" href="https://www.webapp.myfbad.com/Course-Google">คอร์สโฆษณาGoogle</Link></li>
+            <li><Link  className="p" href="https://www.webapp.myfbad.com/Course-Facebook">คอร์สโฆษณาFacebook</Link></li>
+            <li><Link  className="p" href="https://www.webapp.myfbad.com/Interesting">สาระน่ารู้</Link></li>
+            <li><Link  className="p" href="https://www.webapp.myfbad.com/Reviews">รีวิวจากลูกค้าจริง</Link></li>
+            <li><Link  className="p" href="https://www.webapp.myfbad.com/DowloadsGoogle">LandingPage Googleฟรี</Link></li>
+            <li><Link  className="p" href="https://www.webapp.myfbad.com/DowloadsFacebook">LandingPage Facebookฟรี</Link></li>
+            <li><Link  className="p" href="https://www.webapp.myfbad.com/contact">contact</Link></li>
           </ul>
         </div>
         <div className="col-lg-3 py-3">
           <h5>Contact</h5>
           <p>Ads Manager 32 Soi 20 Charoenrat Road, Khlong San District, Bangkok 10160</p>
-          <Link href="tel:0956422872" className="footer-link">0956422872</Link>
-          <Link href="https://www.webapp.myfbad.com" className="footer-link">admin@webapp.myfbad.com</Link>
+          <Link   href="tel:0956422872" className="p footer-link">0956422872</Link>
+          <Link   href="https://www.webapp.myfbad.com" className="p footer-link">admin@webapp.myfbad.com</Link>
         </div>
         <div className="col-lg-3 py-3">
           <h5>www.webapp.myfbad.com</h5>
@@ -49,7 +49,7 @@ function Footer() {
         </div>
       </div>
 
-      <p className="text-center" id="copyright">รับจ้างยิงแอดสายเทา &copy; 2024 รับจ้างยิงแอดสายเทา 2024 <Link href="https://www.webapp.myfbad.com" target="_blank">รับจ้างยิงแอดสายเทา</Link></p>
+      <p className="text-center" id="copyright">รับจ้างยิงแอดสายเทา &copy; 2024 รับจ้างยิงแอดสายเทา 2024 <strong>รับจ้างยิงแอดสายเทา</strong></p>
     </div>
  <p> #รับทำ seo สายเทา, #รับทำ seo สายดำ, #แอดสายเทา, #รับยิงแอดสายเทา, #กูเกิ้ลแอดสายเทา, #รับทํา google ads, #สายเทา seo สายเทา, #รับทำโฆษณาสายเทา, #ทำการตลาดสายเทา, #ads สายเทา, #รับทํา seo สายเทา, #ยิงแอดสายเทา, #โปรโมทสายเทา, #facebook ads, #สายเทา, #การตลาดสายเทา, #ยิงadsสายเทา, #ยิงแอด google สายเทา, #รับทำการตลาดสายเทา,</p>
 <p> #รับจ้างยิงแอดสายเทา, #โฆษณาสายเทา, #ตลาดสายเทา, #ads, #สายเทา, #แอดสายเทา, #ยิงแอดสายเทา, #pantip ยิงแอดสายเทาfacebook, #ทําseoสายเทา, #ยิงadsfacebookสายเทา, #ยิงแอดสายเทาเฮียบอย, #รับทำเว็บสายเทา, #การทำseoสายเทา, #ทำseoสายเทา, #ยิงแอดyoutubeสายเทา,# ยิงแอดสายเทาราคา, #ยิงแอดสายเทา2022, #ยิงแอดสายดำ, #รับจ้างแอดสายเทา, </p>
